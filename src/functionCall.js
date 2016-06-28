6f9404970882e1a1f74c99b39d19448fe3058e71
ug <ned.stuart90@gmail.com>
2016-06-28 13:54:56 -0700

    Updated function calls to correctly handle localname and node name. node.nodeName returns a case insensitive version of the nodes name where localName and prefix return case sensitive versions of the respective qname.

--- src/functionCall.js
+++ src/functionCall.js
@@ -378,7 +378,14 @@ wgxpath.FunctionCall.Func = {
       function(ctx, opt_expr) {
         // TODO: Fully implement this.
         var node = opt_expr ? opt_expr.evaluate(ctx).getFirst() : ctx.getNode();
-        return node ? node.nodeName : '';
+        if(node) {
+          if (goog.isDef(node.localName)) {
+            return (node.prefix ? (node.prefix + ':') : '') + node.localName;
+          } else {
+            return node.nodeName.toLowerCase(); // captial case node name, lower it
+          }
+        }
+        return '';
       }, 0, 1, true),
   NAMESPACE_URI: wgxpath.FunctionCall.createFunc_('namespace-uri',
       wgxpath.DataType.STRING, true, false, false,
